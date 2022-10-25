d4b589b5c090b8588e6d905f5dd5902496abdbbd
M. Khobari Akbar <113977212+khobariAkbar@users.noreply.github.com>
2022-10-25 11:46:21 +0700

    Add files via upload

---
+++ halaman_utama.php
@@ -0,0 +1,12 @@
+<?php
+include 'functions.php';
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p align="center" >SELAMAT DATANG DI MAKUL PEMROGRAMAN WEB 2</p>
+</div>
+
+<?=template_footer()?>
